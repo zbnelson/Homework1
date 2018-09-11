83a2a4118ebb5161ede111298d7e2adfd7781679
zbnelson <42528566+zbnelson@users.noreply.github.com>
2018-09-11 15:35:39 -0500

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/UserInterface.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace Calculator
+{
+    partial class UserInterface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxCompute = new System.Windows.Forms.TextBox();
+            this.uxExpressions = new System.Windows.Forms.TextBox();
+            this.uxSeven = new System.Windows.Forms.Button();
+            this.uxEight = new System.Windows.Forms.Button();
+            this.uxNine = new System.Windows.Forms.Button();
+            this.uxFour = new System.Windows.Forms.Button();
+            this.uxFive = new System.Windows.Forms.Button();
+            this.uxSix = new System.Windows.Forms.Button();
+            this.uxOne = new System.Windows.Forms.Button();
+            this.uxTwo = new System.Windows.Forms.Button();
+            this.uxThree = new System.Windows.Forms.Button();
+            this.uxDecimal = new System.Windows.Forms.Button();
+            this.uxZero = new System.Windows.Forms.Button();
+            this.uxDelete = new System.Windows.Forms.Button();
+            this.uxMinus = new System.Windows.Forms.Button();
+            this.uxMultiply = new System.Windows.Forms.Button();
+            this.uxDivide = new System.Windows.Forms.Button();
+            this.uxEquals = new System.Windows.Forms.Button();
+            this.uxPlus = new System.Windows.Forms.Button();
+            this.uxOpen = new System.Windows.Forms.Button();
+            this.uxClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // uxCompute
+            // 
+            this.uxCompute.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uxCompute.Location = new System.Drawing.Point(12, 12);
+            this.uxCompute.Name = "uxCompute";
+            this.uxCompute.ReadOnly = true;
+            this.uxCompute.Size = new System.Drawing.Size(314, 26);
+            this.uxCompute.TabIndex = 0;
+            this.uxCompute.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // uxExpressions
+            // 
+            this.uxExpressions.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uxExpressions.Location = new System.Drawing.Point(12, 52);
+            this.uxExpressions.Name = "uxExpressions";
+            this.uxExpressions.ReadOnly = true;
+            this.uxExpressions.Size = new System.Drawing.Size(314, 26);
+            this.uxExpressions.TabIndex = 1;
+            this.uxExpressions.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // uxSeven
+            // 
+            this.uxSeven.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxSeven.Location = new System.Drawing.Point(12, 84);
+            this.uxSeven.Name = "uxSeven";
+            this.uxSeven.Size = new System.Drawing.Size(73, 33);
+            this.uxSeven.TabIndex = 2;
+            this.uxSeven.Text = "7";
+            this.uxSeven.UseVisualStyleBackColor = true;
+            this.uxSeven.Click += new System.EventHandler(this.uxSeven_Click);
+            // 
+            // uxEight
+            // 
+            this.uxEight.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uxEight.Location = new System.Drawing.Point(91, 84);
+            this.uxEight.Name = "uxEight";
+            this.uxEight.Size = new System.Drawing.Size(73, 33);
+            this.uxEight.TabIndex = 3;
+            this.uxEight.Text = "8";
+            this.uxEight.UseVisualStyleBackColor = true;
+            this.uxEight.Click += new System.EventHandler(this.uxEight_Click);
+            // 
+            // uxNine
+            // 
+            this.uxNine.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uxNine.Location = new System.Drawing.Point(170, 84);
+            this.uxNine.Name = "uxNine";
+            this.uxNine.Size = new System.Drawing.Size(73, 33);
+            this.uxNine.TabIndex = 4;
+            this.uxNine.Text = "9";
+            this.uxNine.UseVisualStyleBackColor = true;
+            this.uxNine.Click += new System.EventHandler(this.uxNine_Click);
+            // 
+            // uxFour
+            // 
+            this.uxFour.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uxFour.Location = new System.Drawing.Point(12, 123);
+            this.uxFour.Name = "uxFour";
+            this.uxFour.Size = new System.Drawing.Size(73, 32);
+            this.uxFour.TabIndex = 5;
+            this.uxFour.Text = "4";
+            this.uxFour.UseVisualStyleBackColor = true;
+            this.uxFour.Click += new System.EventHandler(this.uxFour_Click);
+            // 
+            // uxFive
+            // 
+            this.uxFive.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uxFive.Location = new System.Drawing.Point(91, 123);
+            this.uxFive.Name = "uxFive";
+            this.uxFive.Size = new System.Drawing.Size(73, 32);
+            this.uxFive.TabIndex = 6;
+            this.uxFive.Text = "5";
+            this.uxFive.UseVisualStyleBackColor = true;
+            this.uxFive.Click += new System.EventHandler(this.uxFive_Click);
+            // 
+            // uxSix
+            // 
+            this.uxSix.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uxSix.Location = new System.Drawing.Point(170, 123);
+            this.uxSix.Name = "uxSix";
+            this.uxSix.Size = new System.Drawing.Size(73, 32);
+            this.uxSix.TabIndex = 7;
+            this.uxSix.Text = "6";
+            this.uxSix.UseVisualStyleBackColor = true;
+            this.uxSix.Click += new System.EventHandler(this.uxSix_Click);
+            // 
+            // uxOne
+            // 
+            this.uxOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uxOne.Location = new System.Drawing.Point(12, 161);
+            this.uxOne.Name = "uxOne";
+            this.uxOne.Size = new System.Drawing.Size(73, 32);
+            this.uxOne.TabIndex = 8;
+            this.uxOne.Text = "1";
+            this.uxOne.UseVisualStyleBackColor = true;
+            this.uxOne.Click += new System.EventHandler(this.uxOne_Click);
+            // 
+            // uxTwo
+            // 
+            this.uxTwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uxTwo.Location = new System.Drawing.Point(91, 161);
+            this.uxTwo.Name = "uxTwo";
+            this.uxTwo.Size = new System.Drawing.Size(73, 32);
+            this.uxTwo.TabIndex = 9;
+            this.uxTwo.Text = "2";
+            this.uxTwo.UseVisualStyleBackColor = true;
+            this.uxTwo.Click += new System.EventHandler(this.uxTwo_Click);
+            // 
+            // uxThree
+            // 
+            this.uxThree.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uxThree.Location = new System.Drawing.Point(170, 161);
+            this.uxThree.Name = "uxThree";
+            this.uxThree.Size = new System.Drawing.Size(73, 32);
+            this.uxThree.TabIndex = 10;
+            this.uxThree.Text = "3";
+            this.uxThree.UseVisualStyleBackColor = true;
+            this.uxThree.Click += new System.EventHandler(this.uxThree_Click);
+            // 
+            // uxDecimal
+            // 
+            this.uxDecimal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uxDecimal.Location = new System.Drawing.Point(12, 199);
+            this.uxDecimal.Name = "uxDecimal";
+            this.uxDecimal.Size = new System.Drawing.Size(73, 31);
+            this.uxDecimal.TabIndex = 11;
+            this.uxDecimal.Text = ".";
+            this.uxDecimal.UseVisualStyleBackColor = true;
+            this.uxDecimal.Click += new System.EventHandler(this.uxDecimal_Click);
+            // 
+            // uxZero
+            // 
+            this.uxZero.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uxZero.Location = new System.Drawing.Point(91, 199);
+            this.uxZero.Name = "uxZero";
+            this.uxZero.Size = new System.Drawing.Size(73, 31);
+            this.uxZero.TabIndex = 12;
+            this.uxZero.Text = "0";
+            this.uxZero.UseVisualStyleBackColor = true;
+            this.uxZero.Click += new System.EventHandler(this.uxZero_Click);
+            // 
+            // uxDelete
+            // 
+            this.uxDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uxDelete.Location = new System.Drawing.Point(170, 233);
+            this.uxDelete.Name = "uxDelete";
+            this.uxDelete.Size = new System.Drawing.Size(156, 34);
+            this.uxDelete.TabIndex = 13;
+            this.uxDelete.Text = "Delete";
+            this.uxDelete.UseVisualStyleBackColor = true;
+            this.uxDelete.Click += new System.EventHandler(this.uxDelete_Click);
+            // 
+            // uxMinus
+            // 
+            this.uxMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uxMinus.Location = new System.Drawing.Point(249, 161);
+            this.uxMinus.Name = "uxMinus";
+            this.uxMinus.Size = new System.Drawing.Size(73, 32);
+            this.uxMinus.TabIndex = 14;
+            this.uxMinus.Text = "-";
+            this.uxMinus.UseVisualStyleBackColor = true;
+            this.uxMinus.Click += new System.EventHandler(this.uxMinus_Click);
+            // 
+            // uxMultiply
+            // 
+            this.uxMultiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uxMultiply.Location = new System.Drawing.Point(249, 123);
+            this.uxMultiply.Name = "uxMultiply";
+            this.uxMultiply.Size = new System.Drawing.Size(73, 32);
+            this.uxMultiply.TabIndex = 15;
+            this.uxMultiply.Text = "x";
+            this.uxMultiply.UseVisualStyleBackColor = true;
+            this.uxMultiply.Click += new System.EventHandler(this.uxMultiply_Click);
+            // 
+            // uxDivide
+            // 
+            this.uxDivide.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uxDivide.Location = new System.Drawing.Point(249, 84);
+            this.uxDivide.Name = "uxDivide";
+            this.uxDivide.Size = new System.Drawing.Size(73, 33);
+            this.uxDivide.TabIndex = 16;
+            this.uxDivide.Text = "/";
+            this.uxDivide.UseVisualStyleBackColor = true;
+            this.uxDivide.Click += new System.EventHandler(this.uxDivide_Click);
+            // 
+            // uxEquals
+            // 
+            this.uxEquals.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uxEquals.Location = new System.Drawing.Point(170, 199);
+            this.uxEquals.Name = "uxEquals";
+            this.uxEquals.Size = new System.Drawing.Size(73, 31);
+            this.uxEquals.TabIndex = 17;
+            this.uxEquals.Text = "=";
+            this.uxEquals.UseVisualStyleBackColor = true;
+            this.uxEquals.Click += new System.EventHandler(this.uxEquals_Click);
+            // 
+            // uxPlus
+            // 
+            this.uxPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uxPlus.Location = new System.Drawing.Point(249, 199);
+            this.uxPlus.Name = "uxPlus";
+            this.uxPlus.Size = new System.Drawing.Size(73, 31);
+            this.uxPlus.TabIndex = 18;
+            this.uxPlus.Text = "+";
+            this.uxPlus.UseVisualStyleBackColor = true;
+            this.uxPlus.Click += new System.EventHandler(this.uxPlus_Click);
+            // 
+            // uxOpen
+            // 
+            this.uxOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uxOpen.Location = new System.Drawing.Point(12, 236);
+            this.uxOpen.Name = "uxOpen";
+            this.uxOpen.Size = new System.Drawing.Size(73, 31);
+            this.uxOpen.TabIndex = 19;
+            this.uxOpen.Text = "(";
+            this.uxOpen.UseVisualStyleBackColor = true;
+            this.uxOpen.Click += new System.EventHandler(this.uxOpen_Click);
+            // 
+            // uxClose
+            // 
+            this.uxClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uxClose.Location = new System.Drawing.Point(91, 236);
+            this.uxClose.Name = "uxClose";
+            this.uxClose.Size = new System.Drawing.Size(73, 31);
+            this.uxClose.TabIndex = 20;
+            this.uxClose.Text = ")";
+            this.uxClose.UseVisualStyleBackColor = true;
+            this.uxClose.Click += new System.EventHandler(this.uxClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 286);
+            this.Controls.Add(this.uxClose);
+            this.Controls.Add(this.uxOpen);
+            this.Controls.Add(this.uxPlus);
+            this.Controls.Add(this.uxEquals);
+            this.Controls.Add(this.uxDivide);
+            this.Controls.Add(this.uxMultiply);
+            this.Controls.Add(this.uxMinus);
+            this.Controls.Add(this.uxDelete);
+            this.Controls.Add(this.uxZero);
+            this.Controls.Add(this.uxDecimal);
+            this.Controls.Add(this.uxThree);
+            this.Controls.Add(this.uxTwo);
+            this.Controls.Add(this.uxOne);
+            this.Controls.Add(this.uxSix);
+            this.Controls.Add(this.uxFive);
+            this.Controls.Add(this.uxFour);
+            this.Controls.Add(this.uxNine);
+            this.Controls.Add(this.uxEight);
+            this.Controls.Add(this.uxSeven);
+            this.Controls.Add(this.uxExpressions);
+            this.Controls.Add(this.uxCompute);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox uxCompute;
+        private System.Windows.Forms.TextBox uxExpressions;
+        private System.Windows.Forms.Button uxSeven;
+        private System.Windows.Forms.Button uxEight;
+        private System.Windows.Forms.Button uxNine;
+        private System.Windows.Forms.Button uxFour;
+        private System.Windows.Forms.Button uxFive;
+        private System.Windows.Forms.Button uxSix;
+        private System.Windows.Forms.Button uxOne;
+        private System.Windows.Forms.Button uxTwo;
+        private System.Windows.Forms.Button uxThree;
+        private System.Windows.Forms.Button uxDecimal;
+        private System.Windows.Forms.Button uxZero;
+        private System.Windows.Forms.Button uxDelete;
+        private System.Windows.Forms.Button uxMinus;
+        private System.Windows.Forms.Button uxMultiply;
+        private System.Windows.Forms.Button uxDivide;
+        private System.Windows.Forms.Button uxEquals;
+        private System.Windows.Forms.Button uxPlus;
+        private System.Windows.Forms.Button uxOpen;
+        private System.Windows.Forms.Button uxClose;
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/UserInterface.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class UserInterface : Form
+    {
+        public UserInterface()
+        {
+            InitializeComponent();
+        }
+
+        private void uxSeven_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uxEight_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uxNine_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uxDivide_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uxFour_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uxFive_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uxSix_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uxMultiply_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uxOne_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uxTwo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uxThree_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uxMinus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uxDecimal_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uxZero_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uxEquals_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uxPlus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uxOpen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uxClose_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uxDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
